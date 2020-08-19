7062c0a667bf19bf699d43858988e859a53f85fc
Amir Mhp <amir.blueb1377@gmail.com>
2020-08-19 23:40:47 +0430

    ref #15 - Memory class, final fields could be static due to their usages. so I made them static to gain a better performance.

--- src/main/java/codeGenerator/Memory.java
+++ src/main/java/codeGenerator/Memory.java
@@ -13,16 +13,16 @@ public class Memory {
 	private List<_3AddressCode> codeBlock;
 	private int lastTempIndex;
 	private int lastDataAddress;
-	private final int stratTempMemoryAddress = 500;
-	private final int stratDataMemoryAddress = 200;
-	private final int dataSize = 4;
-	private final int tempSize = 4;
+	private final static int startTempMemoryAddress = 500;
+	private final static int startDataMemoryAddress = 200;
+	private final static int dataSize = 4;
+	private final static int tempSize = 4;
 	private final PresentationService presenter = new ConsolePresentationServiceImpl();
 
 	public Memory() {
-		codeBlock = new ArrayList<_3AddressCode>();
-		lastTempIndex = stratTempMemoryAddress;
-		lastDataAddress = stratDataMemoryAddress;
+		codeBlock = new ArrayList<>();
+		lastTempIndex = startTempMemoryAddress;
+		lastDataAddress = startDataMemoryAddress;
 	}
 
 	public int getTemp() {
